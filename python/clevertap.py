b22a7f7d5eeac067d0328d7f41a892bb1d8aea99
Peter <peter@clevertap.com>
2017-01-22 11:51:51 -0800

    initial

---
+++ python/clevertap.py
@@ -0,0 +1,369 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2015 CleverTap
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+__all__ = ['CleverTap']
+
+import json
+import urllib
+import urllib2
+
+class CleverTap(object):
+    api_hostname = 'api.clevertap.com'
+    api_version = 1
+
+    class TargetActions(object):
+        CREATE = "create"
+        ESTIMATE = "estimate"
+        LIST = "list"
+        RESULT = "result"
+        STOP = "stop"
+
+        @classmethod
+        def valid_actions(cls):
+            return [cls.CREATE, cls.ESTIMATE, cls.LIST, cls.RESULT, cls.STOP]
+
+    def __init__(self, account_id, account_passcode):
+        self.account_id         = account_id
+        self.account_passcode   = account_passcode
+        self.cursor             = None
+        self.url                = None
+        self.records            = []
+
+    def __repr__(self):
+        return "%s(account_id=%s, passcode=%s)" % (self.__class__.__name__, self.account_id, self.account_passcode)
+
+
+    @property
+    def api_endpoint(self):
+        return 'https://%s/%s' % (self.__class__.api_hostname, self.__class__.api_version)
+
+
+    def upload(self, data):
+        """upload an array of profile and/or event dicts"""
+
+        # validate data
+        validation_error = self._validate("upload", data)
+
+        if validation_error:
+            raise Exception(validation_error)
+            return 
+
+        # construct the base request url
+        self.url = '/'.join([self.api_endpoint, "upload"])
+
+        # the request body is the json encoded data
+        body = json.dumps({"d":data})
+
+        # request headers
+        headers_params = {'Content-Type':'application/json;charset=utf-8'}
+
+        # make the request
+        return self._call(body=body, headers_params=headers_params)
+
+    def targets(self, action, payload):
+
+        # validate 
+        validation_error = self._validate(action, payload)
+
+        if validation_error:
+            raise Exception(validation_error)
+            return
+
+        url_action = None
+
+        if action in self.TargetActions.valid_actions():
+            url_action = action
+
+        if not url_action:
+            print "unknown targets action %s" % action
+            return
+
+        if url_action == self.TargetActions.ESTIMATE:
+            payload['estimate_only'] = True
+            url_action = self.TargetActions.CREATE
+
+        # construct the request url
+        self.url = '/'.join([self.api_endpoint, "targets", "%s.json"%url_action])
+
+        # the request body is the json encoded payload
+        body = json.dumps(payload) if payload else None
+
+        # request headers
+        headers_params = {'Content-Type':'application/json'}
+
+        # make the request
+        return self._call(body=body, headers_params=headers_params)
+
+    def profile(self, email=None, identity=None, objectId=None):
+        """fetch an individual user profile by ID, one of email, identity or CleverTap objectID"""
+
+        if email is None and identity is None and objectId is None:
+            raise Exception("profile requires one of email, identity or objectId")
+            return
+
+        # construct the request url
+        self.url = '/'.join([self.api_endpoint, "profile.json"])
+
+        if email is not None:
+            self.url += "?email=%s" % email
+
+        elif identity is not None:
+            self.url += "?identity=%s" % identity
+
+        elif objectId is not None:
+            self.url += "?objectId=%s" % objectId
+
+        # request headers
+        headers_params = {'Content-Type':'application/json'}
+
+        # make the request
+        return self._call(headers_params=headers_params)
+
+    def profiles(self, query, batch_size=10):
+        """download profiles defined by query"""
+        return self._fetch_records("profiles", query, batch_size=batch_size)
+
+
+    def events(self, query, batch_size=10):
+        """download events defined by query"""
+        return self._fetch_records("events", query, batch_size=batch_size)
+
+
+    def _fetch_records(self, type, query, batch_size=10):
+
+        # reset our records cache
+        self.records = []
+
+        # validate query
+        validation_error = self._validate(type, query)
+
+        if validation_error:
+            raise Exception(validation_error)
+            return
+
+        # construct the base request url
+        self.baseurl = '/'.join([self.api_endpoint, "%s.json"%type])
+
+        _args = urllib.urlencode({"query":json.dumps(query), 'batch_size':batch_size})
+        # add query and batch_size as query args
+        self.url = "%s?%s"%(self.baseurl, _args)
+
+        # request headers
+        headers_params = {'Content-Type':'application/json;charset=utf-8'}
+
+        # make the request
+        res = self._call(headers_params=headers_params) or {}
+
+        # initial request is for the cursor
+        self.cursor = res.get("cursor", None)
+        
+        # if we have a cursor then make a second request with the cursor
+        if self.cursor:
+
+            # construct the request url
+            # add the cursor
+            self.url = "%s?cursor=%s"%(self.baseurl, self.cursor)
+
+
+            # convenience inner function to handle cursor requests 
+            def call_records():
+                # make the request
+                res = self._call() or {}
+
+                # parse response
+                self.cursor = res.get("next_cursor", None)
+                new_records = res.get("records", [])
+
+                # add the new records array to our records array
+                self.records += new_records
+
+                # if the request returns a new cursor, update the api url with the new cursor
+                if self.cursor:
+                    self.url = "%s?cursor=%s"%(self.baseurl, self.cursor)
+                else:
+                    self.url = None
+            
+
+            # keep making requests with the new cursor as long as we have a cursor 
+            while True:
+                if self.cursor == None:
+                    break
+                else:
+                    call_records()
+
+        return self.records
+
+
+    def _call(self, **kwargs):
+
+        if self.url == None:
+            print "api url is None"
+            return None
+
+        headers_params = kwargs.get('headers_params', {}) 
+        # add account_id, and passcode to request headers 
+        headers_params['X-CleverTap-Account-Id'] = self.account_id
+        headers_params['X-CleverTap-Passcode'] = self.account_passcode
+        
+        args = kwargs.get("args", None)
+        if args:
+            args = urllib.urlencode(args)
+
+        body = kwargs.get("body", None)
+
+        # Create the request
+        req = urllib2.Request(self.url, args, headers_params)
+
+        if body:
+            req.add_data(body)
+
+        try:
+            # Open the request
+            f = urllib2.urlopen(req)
+            # Get the response 
+            response = f.read()
+            # Close the opened request
+            f.close()
+
+        except Exception, e:
+            print "%s: %s" % (e, e.read())
+            return None
+
+        # Parse and return the response
+        try:
+            res = self._parse_response(response)
+        except Exception, e:
+            print e
+            res = None
+
+        return res
+            
+    def _parse_response(self, response):
+        """Parse a response from the API"""
+        try:
+            res = json.loads(response)
+        except Exception, e:
+            e.args += ('API response was: %s' % response)
+            raise e
+
+        return res
+
+    def _validate(self, type, data):
+        """Simple data validation"""
+        validation_error = None
+
+        if not self.account_id:
+            validation_error = "clevertap account id missing"
+            return validation_error
+
+        if not self.account_passcode:
+            validation_error = "clevertap account passcode missing"
+            return validation_error
+
+        if type in [self.TargetActions.CREATE, self.TargetActions.ESTIMATE]:
+            if data is None:
+                return "Push targets action %s requires a payload"%type
+
+            if (data.get("name", None) is None):
+                return "Push targets action %s requires a name"%type
+
+            if (data.get("where", None) is None) and (data.get("segment", None) is None):
+                return "Push targets action %s requires a where or segment value"%type
+
+            if (data.get("where", None) is not None) and (data.get("segment", None) is not None):
+                return "Push targets action %s does not support both a where value and a segment value, specify one or the other"%type
+
+            if (data.get("segment", None) is not None):
+                if data.get("segment") != "all":
+                    return "Push targets action %s segment value must be 'all'"%type
+
+            if (data.get("content", None) is None):
+                return "Push targets action %s requires a content dict"%type
+
+            if (data.get("content", None) is not None):
+                if (data.get("content", {}).get("title", None) is None) or (data.get("content", {}).get("body", None) is None): 
+                    return "Push targets action %s content dict requires a title and a body"%type
+
+            if (data.get("devices", None) is None):
+                return "Push targets action %s requires a devices array"%type
+
+            return validation_error
+
+        if type == self.TargetActions.LIST:
+            # no-op
+            return validation_error
+
+        if type in [self.TargetActions.RESULT, self.TargetActions.STOP]:
+            if (data is None) or (data.get("id", None) is None):
+                validation_error = "Push targets %s action requires a target id"%type
+
+            return validation_error
+
+        if type == "upload":
+            for record in data or []:
+
+                identity = record.get("identity", None) or record.get("FBID", None) or record.get("GPID", None) or record.get("objectId", None)
+                if identity is None:
+                    validation_error = "record must contain an identity, FBID, GPID or objectId field: %s"%record
+                    return validation_error
+                    break
+
+                record_type = record.get("type", None)
+                if record_type not in ['profile', 'event']:
+                    validation_error = "record type must be profile or event: %s"%record
+                    return validation_error
+                    break
+
+                if record_type == "profile":
+                    profileData = record.get("profileData", None)
+                    if profileData is None or not isinstance(profileData, dict): 
+                        validation_error = "record with type profile must contain a profileData dict: %s"%record
+                        return validation_error
+                        break
+
+                    Phone = profileData.get("Phone", None)
+                    if Phone and (not isinstance(Phone, str) or not Phone.startswith("+")): 
+                        validation_error = "profile Phone must be a string and start with +<country code>: %s"%record
+                        return validation_error
+                        break
+
+                if record_type == "event":
+                    evtData = record.get("evtData", None)
+                    if evtData is None or not isinstance(evtData, dict): 
+                        validation_error = "record with type event must contain an evtData dict: %s"%record
+                        return validation_error
+                        break
+
+        if type == "profiles":
+            # query = json.loads(data)
+
+            # event_name is a compulsory parameter
+            if "event_name" not in data:
+                validation_error = "Event Name is missing"
+                return validation_error
+
+            # from is a compulsory parameter
+            if "from" not in data:
+                validation_error = "\'From\' is missing"
+                return validation_error
+
+            # to is a compulsory parameter
+            if "to" not in data:
+                validation_error = "\'To\' is missing"
+                return validation_error
+
+        return validation_error
+
---
+++ python/csvdownload.py
@@ -0,0 +1,120 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from clevertap import CleverTap
+import argparse
+import datetime
+import json
+import csv
+
+MAX_BATCH_SIZE = 5000
+
+
+def _flatten(structure, key="", flattened=None):
+    if flattened is None:
+        flattened = {}
+    if type(structure) not in (dict, list):
+        if isinstance(structure, basestring):
+            flattened[key] = "\"" + str(structure.encode("UTF-8")) + "\""
+        else:
+            flattened[key] = str(structure)
+    elif isinstance(structure, list):
+        for i, item in enumerate(structure):
+            _flatten(item, "%d" % i, flattened)
+    else:
+        for new_key, value in structure.items():
+            _flatten(value, new_key, flattened)
+    return flattened
+
+
+def _add_to_field_names(field_names, main_list_of_fields):
+    for field in field_names:
+        if field not in main_list_of_fields:
+            main_list_of_fields.append(field)
+
+
+def _convert_to_csv(result, pathcsv):
+    result_list = []
+    main_list_of_fields = []
+
+    print "Received %s results" % len(result)
+    if len(result) == 0:
+        print "No .csv file created"
+        return
+
+    print "Converting to .csv format..."
+
+    for row in result:
+        try:
+            res = _flatten(row)
+            _add_to_field_names(res.keys(), main_list_of_fields)
+            # res = {k: unicode(v).encode("utf-8") for k,v in res.iteritems()}
+            res = {k: str(v) for k,v in res.iteritems()}
+
+            result_list.append(res)
+        except Exception, e:
+            print e
+            print "ERROR: Failed to process record: %s" % row
+
+    with open(pathcsv, "wb") as csvfile:
+        writer = csv.DictWriter(csvfile, main_list_of_fields)
+        writer.writeheader()
+
+        for row in result_list:
+            try:
+                writer.writerow(row)
+            except Exception, e:
+                print e
+                print "ERROR: Failed to write this record to CSV file: %s" % row
+
+    print "Download Complete!"
+
+
+def main(account_id, passcode, path_json, path_csv, type_of_download, dryrun):
+
+    clevertap = CleverTap(account_id, passcode)
+    result = []
+
+    if type_of_download not in ["event", "profile"]:
+        raise Exception("unknown record type %s" % type)
+        return
+
+    start_time = datetime.datetime.now()
+    print "Downloading..."
+    try:
+        with open(path_json) as data_file:
+                data = json.load(data_file)
+        if type_of_download == "profile":
+            result = clevertap.profiles(data, MAX_BATCH_SIZE)
+        elif type_of_download == "event":
+            result = clevertap.events(data, MAX_BATCH_SIZE)
+        _convert_to_csv(result, path_csv)
+
+    except Exception, e:
+        print e
+
+    finally:
+        end_time = datetime.datetime.now()
+        processing_time = end_time - start_time
+        print "Processing Time: %s" % processing_time
+
+
+if __name__ == "__main__":
+    def str2bool(v):
+        return v.lower() in ("yes", "true", "t", "1")
+
+
+    parser = argparse.ArgumentParser(description='CleverTap CSV downloader')
+    parser.register('type', 'bool', str2bool)
+    parser.add_argument('-a', '--id', help='CleverTap Account ID', required=True)
+    parser.add_argument('-c', '--passcode', help='CleverTap Account Passcode', required=True)
+    parser.add_argument('-pjson', '--pathjson', help='Absolute path to the json file', required=True)
+    parser.add_argument('-pcsv', '--pathcsv', help='Absolute path to the csv file', required=True)
+    parser.add_argument('-t', '--t', help='The type of data, either profile or event, defaults to profile',
+                        default="profile")
+    parser.add_argument('-d', '--dryrun', help='Do a dry run, process records but do not upload', default=False,
+                        type='bool')
+
+    args = parser.parse_args()
+
+    main(args.id, args.passcode, args.pathjson, args.pathcsv, args.t, args.dryrun)
